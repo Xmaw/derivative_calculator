a3afe98d22dd634e80afcc4ee13c66400e21b9d1
Xmaw <ej222up@student.lnu.se>
2018-08-25 13:15:23 +0200

    function added for splitting the polynomial and calculating the derivative of a function of order n without any paranthesis

--- derivative_calculator.py
+++ derivative_calculator.py
@@ -1,5 +1,29 @@
 def break_up_functions(f):
-    base = 
+    base = ""
+    pow = ""
+    flag_pow = False
+    for c in f:
+        if flag_pow:
+            pow = c
+            flag_pow = False
+            break
+
+        elif c == '^':
+            flag_pow = True
+
+        else:
+            base += c
+    print("base:", base)
+    print("pow: ", pow)
+
+    calculate_derivative(base, pow, f)
+
+
+def calculate_derivative(b, p, f):
+    derivative = str(p + b)
+    if int(p) > 2:
+        derivative = derivative + '^' + str(int(p)-1)
+    print("Derivative of ", f, " is: ", derivative)
 
 
 f = input("Enter an input to calculate the derivative of: ")
